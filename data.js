da2d1f8736bc3aa4f0c6a75202e92c680d387f81
laurianeWW <lauriane.lwm@gmail.com>
2026-06-03 08:27:06 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,161 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wenesy_scene-1effectsresult",
+      "name": "wenesy_Scène 1.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6121859012511948,
+          "pitch": 0.39354842685130365,
+          "rotation": 0,
+          "target": "2-wenesy_scene-3effectsresult"
+        },
+        {
+          "yaw": 1.3856969481919919,
+          "pitch": 0.3217213846088427,
+          "rotation": 0,
+          "target": "3-wenesy_scene-4effectsresult"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wenesy_scene-2effectsresult",
+      "name": "wenesy_Scène 2.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wenesy_scene-3effectsresult",
+      "name": "wenesy_Scène 3.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wenesy_scene-4effectsresult",
+      "name": "wenesy_Scène 4.effectsResult",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20348715997242195,
+          "pitch": 0.30002633025634395,
+          "rotation": 0,
+          "target": "1-wenesy_scene-2effectsresult"
+        },
+        {
+          "yaw": -0.7703001838615577,
+          "pitch": 0.2848679776838612,
+          "rotation": 0,
+          "target": "0-wenesy_scene-1effectsresult"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
